4cd9a04037f4254b76f5e1d7a77c09419e7605f9
Hashrocket Workstation <dev@hashrocket.com>
2012-10-04 17:55:32 -0400

    Add rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Luke::Application.config.secret_token = '8f35cbad67dc1deb0095e1a9ebfee2b911b9ead1259b73d2a78d740d2b091aef2a04f0039d1eea7bf8222aae7d605fe21aed6a380acaee6da6b97421b12df6d7'
